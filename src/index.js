ff5d1bd34246eeeebaceddc09401162b47cf4a07
RokasMuningis <rokasmuningis@gmail.com>
2017-11-18 16:52:20 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+export default (props) => {
+  const { auth, component: Component, redirect: pathname, ...rest } = props;
+  
+  return (
+    <Route
+      exact
+      {...rest}
+      render={
+        props => auth ?
+          <Component {...props} /> :
+          <Redirect to={{ pathname, state: {from: props.location} }} />
+      }
+    />
+  );
+}
